ae81dcdb425082934e18827e06320762ce871630
Alviss Raghnall <isiomastewart@gmail.com>
2023-06-21 04:01:57 +0100

    Saint JHN - Gorgeous

--- src/sanity-client.ts
+++ src/sanity-client.ts
@@ -3,11 +3,11 @@ import createImageUrlBuilder from "@sanity/image-url";
 import type {SanityImageSource} from "@sanity/image-url/lib/types/types";
 
 export const client = createClient({
-    projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+    projectId: process.env.SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2023-06-03',
     useCdn: true,
-    token: process.env.NEXT_PUBLIC_SANITY_TOKEN
+    token: process.env.SANITY_TOKEN
 });
 
 export const builder = createImageUrlBuilder(client);
