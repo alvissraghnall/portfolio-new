352bda4d27a7d4d9735f066c2b01dc093e9f6db6
Alviss Raghnall <isiomastewart@gmail.com>
2023-07-21 05:03:31 +0100

    G-Eazy - Random

--- src/app/components/Socials.tsx
+++ src/app/components/Socials.tsx
@@ -1,7 +1,7 @@
 
 import {
     BsTwitter,
-    BsGithub
+    BsGithub, BsLinkedin
 } from 'react-icons/bs';
 import {
     FaFacebookF
@@ -16,6 +16,9 @@ function Socials() {
         </a>
         <a target='_blank' rel='noopener noreferrer' href="https://github.com/alvissraghnall" className="w-10 h-10 rounded-full bg-blanc my-1 mx-0 border border-solid border-light-gray centered-flex transition-all duration-300 ease-in-out group hover:bg-secondary hover:border-secondary 4xl:w-[72px] 4xl:h-[72px] 4xl:my-2 4xl:mx-0 no-underline">
             <BsGithub className='w-4 h-4 text-normal-gray group-hover:text-blanc 4xl:w-7 4xl:h-7' />
+        </a>
+        <a target='_blank' rel='noopener noreferrer' href="https://www.linkedin.com/in/elijah-annie-stewart-456749214" className="w-10 h-10 rounded-full bg-blanc my-1 mx-0 border border-solid border-light-gray centered-flex transition-all duration-300 ease-in-out group hover:bg-secondary hover:border-secondary 4xl:w-[72px] 4xl:h-[72px] 4xl:my-2 4xl:mx-0 no-underline">
+            <BsLinkedin className='w-4 h-4 text-normal-gray group-hover:text-blanc 4xl:w-7 4xl:h-7' />
         </a>
         <a target='_blank' rel='noopener noreferrer' href="https://www.facebook.com/profile.php?id=100089602208618" className="w-10 h-10 rounded-full bg-blanc my-1 mx-0 border border-solid border-light-gray centered-flex transition-all duration-300 ease-in-out group hover:bg-secondary hover:border-secondary 4xl:w-[72px] 4xl:h-[72px] 4xl:my-2 4xl:mx-0 no-underline">
             <FaFacebookF className='w-4 h-4 text-normal-gray group-hover:text-blanc 4xl:w-7 4xl:h-7' />
